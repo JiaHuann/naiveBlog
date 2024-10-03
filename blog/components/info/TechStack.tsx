2a787bf119de76995d5e6140f5d50f576fb7ee86
jiahuan <cheayuki13@gmail.com>
2024-10-04 03:50:49 +0800

    change the gap of stack

--- blog/components/info/TechStack.tsx
+++ blog/components/info/TechStack.tsx
@@ -2,7 +2,7 @@ import {Progress} from "@nextui-org/react";
 
 export default function TechStack() {
   return (
-    <div className="flex flex-col gap-6 w-full max-w-md ">
+    <div className="flex flex-col gap-3 w-full max-w-md ">
       <Progress className="font-mono" color="default" label="Linux Using" value={90} showValueLabel={true} />
       <Progress className="font-mono" color="primary" label="Backend Dev" value={40}  showValueLabel={true}/>
       <Progress className="font-mono" color="secondary" label="Frontend Dev" value={60}  showValueLabel={true}/>
