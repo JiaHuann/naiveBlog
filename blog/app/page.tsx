be114a12114fbfdd4f12845773d8b6842c8da3d5
jiahuan <cheayuki13@gmail.com>
2024-10-07 22:40:40 +0800

    Add plugin github stats

--- blog/app/page.tsx
+++ blog/app/page.tsx
@@ -3,15 +3,17 @@ import MySpacer from "@/components/spacer";
 import TechStack from "@/components/info/TechStack";
 import { Col, Row } from "antd";
 import MyOverlay from "@/components/Overlay"; // 引入遮罩组件
+import HomePageLeft from "@/components/HomePageLeft";
 
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen">
       <MyOverlay />  {/* 添加遮罩组件 */}
-
+      
       <Row className="flex flex-wrap">
         <Col xs={24} sm={12} md={6}>
           <p className="w-full bg-yellow-500">left space</p>
+          <HomePageLeft/>
         </Col>
         <Col xs={24} sm={12} md={12}>
           <p className="w-full bg-blue-500">col-12 col-offset-6</p>
---
+++ blog/components/HomePageLeft.tsx
@@ -0,0 +1,8 @@
+import {Image} from "@nextui-org/react";
+export default function HomePageLeft(){
+    return(
+        <div>
+            <Image src="https://github-readme-stats.vercel.app/api?username=JiaHuann"></Image>
+        </div>
+    )
+}
