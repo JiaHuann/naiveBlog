c0a7178afc174f0d7a8193668aafce82f88c23aa
JiaHuann <cheayuki13@gmail.com>
2025-04-16 02:16:00 +0000

    add MLSys YPS'2025

--- blog/app/aboutme/PDF.tsx
+++ blog/app/aboutme/PDF.tsx
@@ -4,6 +4,7 @@ export default function Test() {
   return (
     <div>
       <iframe src="/assume.pdf" width="100%" height="800px"></iframe>
+      <iframe src="/eBPF_fault_injection_LLM.pdf" width="100%" height="800px"></iframe>
     </div>
   );
 }
